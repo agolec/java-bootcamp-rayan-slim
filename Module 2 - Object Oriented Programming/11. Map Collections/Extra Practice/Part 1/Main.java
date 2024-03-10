694500d614eaa1f70b919d85f5dd2dd9114657fe
Adam <algolec1990@yahoo.com>
2024-03-10 19:34:52 -0400

    add code for part 1 extra practice

--- Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 1/Main.java
+++ Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 1/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        
+        City city1 = new City("New York", "USA", 8500000);
+        System.out.println(city1.getName());
+
+
+        // Uncomment the following line to test invalid input for setName method
+        try {
+            city1.setName("");
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+        // Uncomment the following line to test invalid input for setCountry method
+        try {
+            city1.setCountry("");
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+        // Uncomment the following line to test invalid input for setPopulation method
+         try{
+             city1.setPopulation(-1);
+         } catch (IllegalArgumentException e){
+             System.out.println(e.getMessage());
+         }
+
+
+        // Uncomment the following line to test invalid input for the constructor
+        // City city2 = new City("", "USA", 8500000);
+
     }
 }
