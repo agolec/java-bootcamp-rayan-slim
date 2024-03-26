242457f6b9a0a6a3b3299f0849595963afd076dc
Adam <algolec1990@yahoo.com>
2024-03-26 12:37:24 -0400

    playing with the Numbers.java file.

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Two/Numbers.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Two/Numbers.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Numbers {
     public static void main(String[] args) {
 
         List<Integer> integers = Arrays.asList(25, 19, 23, 45, 38, 23, 59, 12);
+        integers.sort((right,left) -> right.compareTo(left));
+        System.out.println(integers);
 
 
     }
